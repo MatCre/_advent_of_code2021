532be32e3549f9d39a7148616259fd110c2efacc
Mathew Donley <m.donley001@gmail.com>
2021-12-08 20:23:15 +0000

    Diagonal Line Positions Calculated

--- 05/hyrothermal_venture.py
+++ 05/hyrothermal_venture.py
@@ -16,6 +16,35 @@ def coords_between_points(x,y):
     elif x > y:
         return [i for i in range(x, y-1, -1)]
 
+def coord_between_points_diagonal(start_x,end_x,start_y,end_y):
+    start_x = int(start_x)
+    end_x = int(end_x)
+    start_y = int(start_y)
+    end_y = int(end_y)
+    x_points = []
+    y_points = []
+    print(f"From {start_x, start_y} to {end_x, end_y}")
+
+    if start_x < end_x:
+        for i in range(0,end_x - start_x + 1):
+            x_points.append(start_x + i)
+    elif start_x > end_x:
+        for i in range(0,start_x - end_x + 1):
+            x_points.append(start_x - i)
+    
+    if start_y < end_y:
+        for i in range(0,end_y - start_y + 1):
+            y_points.append(start_y + i)
+    elif start_y > end_y:
+        for i in range(0,start_y - end_y + 1):
+            y_points.append(start_y - i)
+    combined = []
+    for i in range(len(x_points)):
+        combined.append((x_points[i],y_points[i]))
+
+    return combined
+        
+
 
 pattern = r"(\d,\d) -> (\d,\d)"
 with open("./05/data.txt", "r") as file:
@@ -51,5 +80,8 @@ for lines in line_data:
         print(coords)
     else:
         print("Diagonal Line")
+        coords = coord_between_points_diagonal(start_x,end_x, start_y,end_y)
+        print(coords)
+        #For diagonal lines increment / decrement betwwen start x and end x and start y and end y 8,0 0,8 -> 7,1 6,2 5,3 4,4 3,5 2,6 1,7 0,8
 
 
